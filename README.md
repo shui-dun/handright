e371ff3b4b36fff9be7956fa057f42a2b1faf9b2
shui-dun <2604697844@qq.com>
2020-09-30 19:37:20 +0800

    完成了项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# handright
+
+使用开源的handright库，抄毛概笔记
---
+++ a.py
@@ -0,0 +1,31 @@
+from multiprocessing import Pool
+from PIL import Image, ImageFont
+from handright import Template, handwrite
+
+if __name__ == '__main__':
+    size = 120
+    with open('text.txt', encoding='utf8') as f:
+        text = f.read()
+    template = Template(
+        background=Image.open(r'bg.png'),
+        font_size=size,
+        font=ImageFont.truetype(r"hxk2.ttf"),
+        line_spacing=int(1.5 * size),
+        fill=0,
+        left_margin=int(0.5 * size),
+        top_margin=int(size * 1.2),
+        right_margin=int(0.5 * size),
+        bottom_margin=int(size * 1.2),
+        word_spacing=0,
+        line_spacing_sigma=0.06 * size,
+        font_size_sigma=0.05 * size,
+        word_spacing_sigma=0.03 * size,
+        end_chars=',.?!，。？！、‘“；：',
+        perturb_x_sigma=0.02 * size,
+        perturb_y_sigma=0.02 * size,
+        perturb_theta_sigma=0.0005 * size,
+    )
+    with Pool() as p:
+        images = handwrite(text, template, mapper=p.map)
+        for i, im in enumerate(images):
+            im.save("output/{}.jpg".format(i))
